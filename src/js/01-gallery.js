3da0af7a01227df49e5cd91485d1ead00621c7f4
orch20 <94862153+orch20@users.noreply.github.com>
2022-05-25 00:05:26 +0300

    Installed and imported SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const renderGallery = () =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            </a>`,
+    )
+    .join('');
+
+refs.gallery.insertAdjacentHTML('afterbegin', renderGallery());
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
