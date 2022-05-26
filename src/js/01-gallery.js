7606af122ac75c2db1d2047f30472dc194ad700b
orch20 <94862153+orch20@users.noreply.github.com>
2022-05-26 23:28:12 +0300

    imported SimpleLightbox (task 1)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,6 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
-
-console.log(galleryItems);
 
 const refs = {
   gallery: document.querySelector('.gallery'),
