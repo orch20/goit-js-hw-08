5ee38b43d9bfa07310e32542493a87aab71f9f3e
orch20 <94862153+orch20@users.noreply.github.com>
2022-05-27 21:44:42 +0300

    added throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function (seconds) {
+  seconds = 0;
+});
+
+const throttledTimeUpdate = throttle(timeUpdate, 1000);
+
+function timeUpdate({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+}
+
+player.on('timeupdate', throttledTimeUpdate);
